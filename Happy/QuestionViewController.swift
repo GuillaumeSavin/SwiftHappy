11f3e47d9aba5c66989c0c6817d6735e8ffd29d1
GuillaumeSavin <guisav@hotmail.fr>
2021-03-15 01:23:05 -0700

    add sage support on question controller

--- Happy/QuestionViewController.swift
+++ Happy/QuestionViewController.swift
@@ -16,14 +16,24 @@ class QuestionViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let hvc = segue.destination as? HappinessViewController {
+            if let identifier = segue.identifier {
+                case "cool":
+                hvc.allegresse = 100
+                case "bof":
+                hvc.alegresse = 50
+                case "nul":
+                hvc.alegresse = 0
+            }
+        }
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
